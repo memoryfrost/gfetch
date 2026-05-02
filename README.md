ee8127128a15fcf9170fe225a035a6b6a613019d
xelframe <xelframe0@gmail.com>
2026-05-02 21:26:08 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gfetch
-Minimal system info fetch written in Go
+
+### Minimal system info fetch written in Go
+### Made for the purpose of studying Go
+
+### Supported distros
+
+- Arch Linux
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/jaypipes/ghw"
+
+	"github.com/shirou/gopsutil/v4/cpu"
+	"github.com/shirou/gopsutil/v4/host"
+	"github.com/shirou/gopsutil/v4/mem"
+)
+
+func main() {
+	ram, _ := mem.VirtualMemory()
+	cpu, _ := cpu.Info()
+	gpu, _ := ghw.GPU()
+	hostname, _ := os.Hostname()
+	username, _ := user.Current()
+	hostinfo, _ := host.Info()
+	var distro string
+	switch hostinfo.Platform {
+	case "arch":
+		distro = "Arch Linux"
+	}
+	fmt.Println(`    .--.
+   |o_o |
+   |:_/ |
+  //   \ \
+ (|     | )
+/'\\_   _/'\
+\___)=(___/`)
+	fmt.Println("---------------")
+	fmt.Printf("%s@%s\n", username.Username, hostname)
+	fmt.Println("OS:", distro, hostinfo.KernelArch)
+	fmt.Println("Kernel:", hostinfo.KernelVersion)
+	fmt.Println("CPU:", cpu[0].ModelName)
+	for _, card := range gpu.GraphicsCards {
+		fmt.Println("GPU:", card.DeviceInfo.Product.Name)
+	}
+	fmt.Println("RAM:", ram.Used/1024/1024, "MiB /", ram.Total/1024/1024, "MiB")
+}
