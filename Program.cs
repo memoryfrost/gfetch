32500e706a4321f0cbc8f4f28ed8577ffaee9d6a
memoryfrost <xelframe0@gmail.com>
2026-09-01 14:07:21 +0300

    rewrited

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.Intrinsics.X86;
+using System.Management;
+
+PlatformID platform = Environment.OSVersion.Platform;
+
+if (platform != PlatformID.Win32NT)
+{
+    Console.WriteLine("Program works only on Windows");
+    Environment.Exit(0);
+}
+
+switch (platform)
+{
+    case PlatformID.Win32NT:
+        Console.WriteLine("       _.-;;-._\n" +
+            "'-..-'|   ||   |\n" +
+            "'-..-'|_.-;;-._|\n" +
+            "'-..-'|   ||   |\n" +
+            "'-..-'|_.-''-._|");
+        break;
+    default:
+        Console.WriteLine("Unknown system");
+        break;
+}
+Console.WriteLine("---------------");
+Console.WriteLine($"{Environment.UserName}@{Environment.MachineName}");
+Console.WriteLine($"OS: {Environment.OSVersion}");
+//Console.WriteLine($"Kernel: ");
+switch (platform)
+{
+    case PlatformID.Win32NT:
+        using (var mos = new ManagementObjectSearcher("SELECT Name FROM Win32_Processor"))
+            foreach (ManagementObject mo in mos.Get())
+                Console.WriteLine("CPU: " + mo["Name"]);
+        using (var mos = new ManagementObjectSearcher("SELECT Name FROM Win32_VideoController"))
+            foreach (ManagementObject mo in mos.Get())
+                Console.WriteLine("GPU: " + mo["Name"]);
+        using (var mos = new ManagementObjectSearcher("SELECT TotalVisibleMemorySize, FreePhysicalMemory FROM Win32_OperatingSystem"))
+            foreach (ManagementObject mo in mos.Get())
+                Console.WriteLine("RAM: " + ((ulong)mo["TotalVisibleMemorySize"] - (ulong)mo["FreePhysicalMemory"]) / 1024 + "MiB / " + (ulong)mo["TotalVisibleMemorySize"] / 1024 + "MiB");
+        break;
+    default:
+        Console.WriteLine("CPU: Not supported on " + platform);
+        Console.WriteLine("GPU: Not supported on " + platform);
+        Console.WriteLine("RAM: Not supported on " + platform);
+        break;
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# gfetch
+
+### Minimal system info fetch written in C#
+
+### Made for the purpose of studying C#
+
+### Supported:
+
+- Windows
